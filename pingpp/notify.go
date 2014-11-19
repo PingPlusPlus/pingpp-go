36649f162a49fc352a1f2a976dd5000b517e3627
Lekton <lekton.zhang@pingplusplus.com>
2014-11-19 12:50:42 +0800

    Bug Fixes

--- pingpp/notify.go
+++ pingpp/notify.go
@@ -4,26 +4,33 @@ import (
 	"encoding/json"
 )
 
-
 type ObjectIndentify struct {
 	object string `json:object`
 }
 
-func parseNotify(notifyJson string){
+func parseNotify(notifyJson string) interface{} {
 	var identify ObjectIndentify
 	var charge Charge
 	var refund Refund
-	err := json.Unmarshal(notifyJson, &identify)
+	err := json.Unmarshal([]byte(notifyJson), &identify)
+
 	if err != nil {
 		return nil
 	}
 	if identify.object == "charge" {
-		err2 := json.Unmarshal(notifyJson, &charge)
-		return &charge
-	}
-	else if identify.object == "refund" {
-		err2 := json.Unmarshal(notifyJson, &refund)
-		return &refund
+		err2 := json.Unmarshal([]byte(notifyJson), &charge)
+		if err2 != nil {
+			return &charge
+		} else {
+			return nil
+		}
+	} else if identify.object == "refund" {
+		err2 := json.Unmarshal([]byte(notifyJson), &refund)
+		if err2 != nil {
+			return &refund
+		} else {
+			return nil
+		}
 	}
 	return nil
 }
