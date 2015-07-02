cb98f4cb144190d4d5f1e9fea18c8d124382924a
shenlin <lumixiliu@163.com>
2015-07-02 12:04:07 +0800

    更改微信红包的部分字段

--- pingpp/redenvelope.go
+++ pingpp/redenvelope.go
@@ -19,20 +19,23 @@ type RedEnvelopeListParams struct {
 }
 
 type RedEnvelope struct {
-	Id          string                 `json:"id"`
-	Object      string                 `json:"object"`
-	Created     uint64                 `json:"created"`
-	Livemode    bool                   `json:"livemode"`
-	App         string                 `json:"app"`
-	Channel     string                 `json:"channel"`
-	Order_no    string                 `json:"order_no"`
-	Amount      uint64                 `json:"amount"`
-	Currency    string                 `json:"currency"`
-	Recipient   string                 `json:"recipient"`
-	Subject     string                 `json:"subject"`
-	Body        string                 `json:"body"`
-	Description string                 `json:"description"`
-	Extra       map[string]interface{} `json:"extra"`
+	Id             string                 `json:"id"`
+	Object         string                 `json:"object"`
+	Created        uint64                 `json:"created"`
+	Received       uint64                 `json:"received"`
+	Livemode       bool                   `json:"livemode"`
+	Status         string                 `json:"status"`
+	App            string                 `json:"app"`
+	Channel        string                 `json:"channel"`
+	Order_no       string                 `json:"order_no"`
+	Transaction_no string                 `json:"transaction_no"`
+	Amount         uint64                 `json:"amount"`
+	Currency       string                 `json:"currency"`
+	Recipient      string                 `json:"recipient"`
+	Subject        string                 `json:"subject"`
+	Body           string                 `json:"body"`
+	Description    string                 `json:"description"`
+	Extra          map[string]interface{} `json:"extra"`
 }
 
 type RedEnvelopeList struct {
@@ -44,7 +47,7 @@ type RedEnvelopeExtra struct {
 	Nick_name     string `json:"nick_name"`
 	Send_name     string `json:"send_name"`
 	Logo          string `json:"logo,omitempty"`
-	Share_url     string `json:"share_url.omitempty"`
+	Share_url     string `json:"share_url,omitempty"`
 	Share_content string `json:"share_content,omitempty"`
 	Share_img     string `json:"share_img,omitempty"`
 }
