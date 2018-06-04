5648e50c4cb584a971cbe4c565ba185c369860e7
yunzhouwu <yunzhouwu@pingxx.com>
2018-06-04 16:01:46 +0800

    rename user_name to name

--- demo/common/withdrawal/wx_pub.go
+++ demo/common/withdrawal/wx_pub.go
@@ -3,5 +3,5 @@ package withdrawal
 var Wx_pub = map[string]interface{}{
 	"open_id":     "wxopenid", //必填参数 收款人的 open_id。
 	"name":        "张三",       //必填参数 收款人姓名。
-	"force_check": false,      //选填参数 是否强制校验收款人姓名。仅当 user_name 参数不为空时该参数生效。
+	"force_check": false,      //选填参数 是否强制校验收款人姓名。仅当 name 参数不为空时该参数生效。
 }
